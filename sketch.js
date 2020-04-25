99de6711be65b5f50c84bdd61c0219ee212adb7c
getuliovale <60718233+getuliovale@users.noreply.github.com>
2020-04-25 00:09:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  
+createCanvas(400, 400);
+  
+background(220);
+  
+  
+var nn = new RedeNeural(1,3,1);
+  
+var arr = [1,2];
+  
+nn.feedforward(arr)
+}
+
+
+function draw() {
+  
+
+}
